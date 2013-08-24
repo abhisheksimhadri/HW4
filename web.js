cec6fa4750d43ee77c8f69f01f281610f27ae7d0
Abhishek Simhadri <abhishek.simhadri@aristolean.com>
2013-08-24 20:01:29 +0000

    Added the CSS Styling to site from Twitter Bootstrap

---
+++ web.js
@@ -0,0 +1,18 @@
+
+var buf = new Buffer(250);
+var string;
+var fs = require('fs');
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+buf =  fs.readFileSync('index1.html', 'utf-8');
+string = buf.toString('utf8', 0, buf);
+response.send (string);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
